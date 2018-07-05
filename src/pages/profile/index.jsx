9d6cee694d1d27b6691d3681a8578fde2aee360f
lixuanqi <lixuanqi1995@gmail.com>
2018-07-05 23:17:05 +0800

    make profile page responsive.

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Row, Col, Button, Divider, Icon } from 'antd';
+import { Row, Col, Button, Divider, Icon, Table } from 'antd';
 import styles from './index.css';
 import avatar from '../../assets/avatar4.jpg';
 import UserInfoCasesTabel from 'Components/UserInfoCasesTabel.jsx';
 import UserInfoStatusTable from 'Components/UserInfoStatusTable.jsx';
+
 import Link from 'umi/link'
+
+
 class ProfilePage extends React.Component {
   componentDidMount() {
     this.props.dispatch({
@@ -14,15 +17,31 @@ class ProfilePage extends React.Component {
   }
   render() {
     const { profile, userDetail, userCases } = this.props;
+
+    const dataSource = [{
+      key: '1',
+      username: profile.username,
+      email: profile.email,
+    }];
+    
+    const columns = [{
+      title: '用户名',
+      dataIndex: 'username',
+      key: 'username',
+    }, {
+      title: '邮箱',
+      dataIndex: 'email',
+      key: 'email',
+    }];
+
     return (
       <div>
         <Row className={styles.container} type="flex" justify="center" gutter={24}>
   
-          <Col span={5} className={styles.sideBar}>
+          <Col xs={0} sm={5} className={styles.sideBar}>
             <div className={styles.avatarContainer}>
               <img alt="avatar" src={avatar} width="100%" />
             </div>
-  
             <span className={styles.username}>
               {profile.username}
             </span>
@@ -31,7 +50,17 @@ class ProfilePage extends React.Component {
             <Icon style={{ marginRight: '8px' }} type="mail" />
             <span className={styles.email}>{profile.email}</span>
           </Col>
-          <Col span={16} >
+          <Col xs={22} sm={16} >
+            <div className={styles.mobileInfo}>
+              <div className={styles.headerContainer}>
+                <h2>用户信息</h2>
+                <Link to="/user_report"><Button style={{ width: '100%' }}>报三维</Button></Link>
+              </div>
+              <div className={styles.contentContainer}>
+                <Table dataSource={dataSource} columns={columns} />
+              </div>
+            </div>
+
             <div className={styles.headerContainer}>
               <h2>你的录取汇报</h2>
               <Link to="/case_report"><Button>报Offer</Button></Link>
